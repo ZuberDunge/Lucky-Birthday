6b9f9ba273677d827cbb2df8a1f8ff2837c55da0
ZuberDunge <iamZuberDunge@gmail.com>
2021-09-13 11:17:17 +0530

    update error mssg

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
+const errorBox = document.querySelector("#error-box");
 var loadingImg  = document.querySelector('.loading-img');
 
 function compareVlues(sum, luckyNumber){
     if(sum%luckyNumber===0){
 
         setTimeout(function(){  
+           
             loadingImg.style.display = `none`;       
             outputBox.style.display = `block`;    
             outputBox.innerText="Yayyyy!! Your birthday is lucky";
             }, 3000);
-    
+            errorBox.style.display = `none`;
             loadingImg.style.display = `block`;      
             outputBox.innerText = 'Hang on our robot is working on it!';
             outputBox.style.display = `block`;
@@ -25,12 +27,13 @@ function compareVlues(sum, luckyNumber){
     }else{
 
         setTimeout(function(){  
+           
             loadingImg.style.display = `none`;       
             outputBox.style.display = `block`;    
             
         outputBox.innerText="Aww Your birthday is not lucky ( but You're <3)";
             }, 3000);
-    
+            errorBox.style.display = `none`;
             loadingImg.style.display = `block`;      
             outputBox.innerText = 'Hang on our robot is working on it!';
             outputBox.style.display = `block`;
@@ -49,10 +52,13 @@ function compareVlues(sum, luckyNumber){
 function checkLuckyBirthday() {
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
-    if(sum&&dob)
-    compareVlues(sum, luckyNumber.value);
+    const luckNumber = Number(luckyNumber.value);
+    if(sum&&dob&&luckNumber)
+    compareVlues(sum, dob, luckNumber);
+    
     else
-    outputBox.innerText="Please Enter both fields!"
+    errorBox.style.display = `block`;
+    errorBox.innerText="Please Enter both fields!"
 }
 
 function calculateSum(dob) {
