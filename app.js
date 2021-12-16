02dd10153e6993258a119522014e791dad6c9a64
ZuberDunge <iamZuberDunge@gmail.com>
2021-12-16 07:13:45 +0530

    Update UI

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
+const username = document.querySelector("#name");
 const checkNumber = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
+const notluckyname = document.querySelector(".you-not-lucky")
 const errorBox = document.querySelector("#error-box");
-var loadingImg  = document.querySelector('.loading-img');
+var loadingImg = document.querySelector('.loading-img');
+var loadingTxt = document.querySelector('.loading-txt');
+var Blur = document.querySelector('.blur');
+const closeButton = document.querySelectorAll(".reset-button");
+const imgDiv = document.querySelector(".imgDiv");
 
-function compareVlues(sum, luckyNumber){
-    if(sum%luckyNumber===0){
+function compareVlues(sum, luckyNumber) {
 
-        setTimeout(function(){  
-           
-            loadingImg.style.display = `none`;       
-            outputBox.style.display = `block`;    
-            outputBox.innerText="Yayyyy!! Your birthday is lucky";
-            }, 3000);
-            errorBox.style.display = `none`;
-            loadingImg.style.display = `block`;      
-            outputBox.innerText = 'Hang on our robot is working on it!';
+    if (sum % luckyNumber === 0) {
+
+        setTimeout(function () {
+            loadingImg.style.display = `none`;
             outputBox.style.display = `block`;
+            loadingTxt.innerText = "";
+            Blur.style.filter = "blur(10px)"
+            notluckyname.innerHTML = `Yayyyy!! <span>${username.value} </span><br> Your birthday is lucky`;
+            imgDiv.innerHTML = ` <img src="./lucky.svg">`
+        }, 2000);
+        errorBox.style.display = `none`;
+        loadingImg.style.display = `block`;
+        loadingTxt.innerText = `Hang on we're  working on it!`;
+        Blur.style.filter = "blur(10px)"
+    } else {
 
-
-        
-
-
-
-    }else{
-
-        setTimeout(function(){  
-           
-            loadingImg.style.display = `none`;       
-            outputBox.style.display = `block`;    
-            
-        outputBox.innerText="Aww Your birthday is not lucky ( but You're <3)";
-            }, 3000);
-            errorBox.style.display = `none`;
-            loadingImg.style.display = `block`;      
-            outputBox.innerText = 'Hang on our robot is working on it!';
+        setTimeout(function () {
+            loadingImg.style.display = `none`;
             outputBox.style.display = `block`;
-
-
-        
-
-
-
-
+            loadingTxt.innerText = "";
+            Blur.style.filter = "blur(10px)"
+            notluckyname.innerHTML = `Sorry!! <span>${username.value} </span><br>Your birthday is not lucky,<br><span>but you're 💕</span> `;
+            imgDiv.innerHTML = ` <img src="./luckno-y.svg">`
+        }, 2000);
+        errorBox.style.display = `none`;
+        loadingImg.style.display = `block`;
+        loadingTxt.innerText = `Hang on we're  working on it!`;
+        Blur.style.filter = "blur(10px)"
 
     }
 }
@@ -53,12 +50,13 @@ function checkLuckyBirthday() {
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
     const luckNumber = Number(luckyNumber.value);
-    if(sum&&dob&&luckNumber)
-    compareVlues(sum, luckNumber);
-    
+    if (username.value && sum && dob && luckNumber > 0)
+        compareVlues(sum, luckNumber);
+
     else
-    errorBox.style.display = `block`;
-    errorBox.innerText="Please Enter both fields!"
+        errorBox.style.display = `block`;
+    errorBox.innerText = "Please Enter All fields with valid inputs!"
+
 }
 
 function calculateSum(dob) {
@@ -68,8 +66,19 @@ function calculateSum(dob) {
         sum = sum + Number(dob.charAt(index));
     }
     return sum;
-
 }
 
 
 checkNumber.addEventListener('click', checkLuckyBirthday)
+
+closeButton.forEach(item => {
+    item.addEventListener("click", () => {
+        username.value = "";
+        luckyNumber.value = "";
+        dateOfBirth.value = "";
+        Blur.style.filter = "none";
+        outputBox.style.display = "none";
+        console.log("clicked");
+
+    })
+})
